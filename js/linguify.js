e0878eeb125bc1288157d00593c3a7caaf03029e
Amr El Aswar <amroali94@gmail.com>
2019-11-17 21:53:38 +0100

    license

--- js/linguify.js
+++ js/linguify.js
@@ -1,3 +1,25 @@
+/**
+
+Linguify a multi-language website managment library
+
+Copyright (C) 2019 Brakets SRL
+
+Author : Amr El Aswar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/> 
+
+**/
+
 function Linguify(){
     var loadedCallbacks = [];
     var loadedAllText = false;
